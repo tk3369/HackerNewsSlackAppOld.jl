9422b8ab4ed2d3e0d76041e230d8cce98431bc5b
Tom Kwong <tk3369@gmail.com>
2019-11-21 07:09:57 +0000

    Files generated by PkgTemplates

---
+++ src/HackerNewsSlackApp.jl
@@ -0,0 +1,5 @@
+module HackerNewsSlackApp
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HackerNewsSlackApp
+using Test
+
+@testset "HackerNewsSlackApp.jl" begin
+    # Write your own tests here.
+end
